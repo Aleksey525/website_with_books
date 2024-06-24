96865b8aff703bab58b215c14a945ff72ff96e28
Aleksey525 <alex_tolchin@mail.ru>
2024-06-24 22:04:52 +0300

    encoded links

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 import os
+import urllib.parse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -16,11 +17,21 @@ def on_reload(books):
     os.makedirs(folder, exist_ok=True)
     divide_pages = list(chunked(books, NUMBER_OF_BOOKS))
     for page_number, books in enumerate(divide_pages):
+        encode_path(books)
         template = env.get_template('template.html')
         rendered_page = template.render(books=books, page_number=page_number, number_of_pages=len(divide_pages))
         complete_path = os.path.join(folder, f'index{page_number + 1}.html')
         with open(complete_path, 'w', encoding="UTF-8") as file:
             file.write(rendered_page)
+
+
+def encode_path(books):
+    folder = '../media/'
+    for book in books:
+        image_path = os.path.join(folder, book['img_src'])
+        text_path = os.path.join(folder, book['book_path'])
+        book['img_src'] = urllib.parse.quote(image_path)
+        book['book_path'] = urllib.parse.quote(text_path)
 
 
 def rebuild():
